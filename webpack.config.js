4857e486771277f03312d5a617b5f75e9ce03c44
kwonsean <gownn12@naver.com>
2021-10-26 18:13:47 +0900

    feat: Add plugin to link html file
    - html파일을 다루기 위하여 plugins을 추가해주었습니다.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-//node.js환경이기 때문에 module.exports를 이용한다.
+//node.js환경이기 때문에 module.exports, require를 이용한다.
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = (env, options) => { 
   return{
     entry:'./src/main.js', // webpack의 진입점 설정 (객체로 만들면 여러개의 진입점을 만들 수 있다.)
@@ -6,6 +8,11 @@ module.exports = (env, options) => {
       // 두개 모두 생략 가능하다. 
       // path:'', 
       // filename:''
-    }
+    },
+    plugins:[
+      new HtmlWebpackPlugin({
+        template: './src/index.html' // 가져올 html 경로
+      })
+    ]
   }
 }  
